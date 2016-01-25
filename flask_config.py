45150a4c32b7b7b0e0e0216a3102c8f7225abb6a
Pawel Lagodzinski <Pawel.Lagodzinski@sas.com>
2016-01-25 22:18:02 +0100

    SQLAlchemy dodana w całości

--- flask_config.py
+++ flask_config.py
@@ -11,3 +11,6 @@ SECRET_KEY = '938455004849506044335'
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
